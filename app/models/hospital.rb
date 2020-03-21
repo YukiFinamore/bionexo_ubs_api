aac9dc809318c4c6e81d6d07923ce82fbd6b8428
Yuki Finamore <yuki@loldesign.com.br>
2020-03-21 16:42:56 -0300

    Create methods to upload csv file

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,16 +1,27 @@
 class Hospital < ApplicationRecord
   require 'csv'
-  
-  validates :name,
-            :address,
-            :city,
-            :phone,
-            :geocode_lat,
+
+  validates :geocode_lat,
             :geocode_long, presence: true
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      Hospital.create! row.to_hash
+      Hospital.create! build_hospital(row)
     end
+  end
+
+  def self.build_hospital(ubs)
+    {
+      name: ubs["no_fantasia"],
+      address: build_address(ubs),
+      city: "#{ubs["uf"]} - #{ubs["cidade"]}",
+      phone: ubs["nu_telefone"],
+      geocode_lat: ubs["lat"],
+      geocode_long: ubs["long"]
+    }
+  end
+
+  def self.build_address(address)
+    "#{address["no_logradouro"]}, #{address["nu_endereco"]} - #{address["no_bairro"]}"
   end
 end
