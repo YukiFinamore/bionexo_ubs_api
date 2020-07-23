330980388c5eb9ba20a8243e4a10054c06070eec
Yuki Finamore <yuki@loldesign.com.br>
2020-07-22 23:41:22 -0300

    Update Hospital FactoryBot

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
-	factory :ubs, class: Hospital do
+	factory :hospital, class: Hospital do
     name { Faker::Name.name }
     address { Faker::Address.street_address }
-    city { Faler::Address.city }
+    city { Faker::Address.city }
     phone { Faker::PhoneNumber.phone_number }
-    geocode_lat { Faker::Number.decimal(l_digits: 2, r_digits: 7) #=> 181.843 }
-    geocode_long { Faker::Number.decimal(l_digits: 2, r_digits: 7) #=> 181.843 }
+    geocode_lat { rand(-90.0..90.0) }
+    geocode_long { rand(-180.0..180.0) }
   end
 end
