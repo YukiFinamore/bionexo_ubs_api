ebabb3dec3670fbb08b942419001e4ddafc8aadd
Yuki Finamore <yuki@loldesign.com.br>
2020-07-23 00:20:07 -0300

    Create hospitals controller spec

---
+++ spec/controllers/api/v1/hospitals_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'pry'
+
+describe Api::V1::HospitalsController, type: :controller do
+  describe 'GET #index' do
+    let!(:hospitals) { create_list(:hospital, 5, geocode_lat: "-23.5428363", geocode_long: "-46.637257") }
+
+    before do
+      get :index
+    end
+
+    context 'hospital methods successful' do
+      let!(:parse_response) { JSON.parse(response.body) }
+      let(:serialized_items) do
+        ActiveModel::Serializer::CollectionSerializer.new(hospitals, serializer: HospitalSerializer).to_json
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(parse_response).to match_array(JSON.parse(serialized_items)) }
+      it { expect(parse_response.count).to eq(5) }
+    end
+  end
+end
