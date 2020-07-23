33548aa2e2d06335068205d75717f7d61fd00440
Yuki Finamore <yuki@loldesign.com.br>
2020-07-23 00:39:05 -0300

    Update Api V1 Hospital Controller Spec

--- spec/controllers/api/v1/hospitals_controller_spec.rb
+++ spec/controllers/api/v1/hospitals_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 require 'pry'
 
 describe Api::V1::HospitalsController, type: :controller do
-  describe 'GET #index' do
-    let!(:hospitals) { create_list(:hospital, 5, geocode_lat: "-23.5428363", geocode_long: "-46.637257") }
+  let!(:hospitals) { create_list(:hospital, 5, geocode_lat: "-23.5428363", geocode_long: "-46.637257") }
+  let(:parse_response) { JSON.parse(response.body) }
 
+  describe 'GET #index' do
     context 'hospital methods successful' do
       before { get :index }
 
-      let!(:parse_response) { JSON.parse(response.body) }
       let(:serialized_items) do
         ActiveModel::Serializer::CollectionSerializer.new(hospitals, serializer: HospitalSerializer).to_json
       end
@@ -17,5 +17,28 @@ describe Api::V1::HospitalsController, type: :controller do
       it { expect(parse_response).to match_array(JSON.parse(serialized_items)) }
       it { expect(parse_response.count).to eq(5) }
     end
+  end
+
+  describe 'GET #find_by_term' do
+    let(:params) { {search: "john"} }
+
+    let!(:hospital_john_doe) do 
+      create(
+        :hospital,
+        name: "John Doe", 
+        geocode_lat: "-23.5428363", 
+        geocode_long: "-46.637257"
+      ) 
+    end
+
+    before { get :find_by_term, params: params }
+
+    context 'find John Doe' do
+      let(:serialized_items) do
+        ActiveModel::Serializer::CollectionSerializer.new([hospital_john_doe], serializer: HospitalSerializer).to_json
+      end
+
+      it { expect(parse_response).to match_array(JSON.parse(serialized_items)) }
+    end 
   end
 end
